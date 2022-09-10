9f7ce00c59b848753cb0e76c8bab99cc1331b012
eirineo <60438888+eirineo@users.noreply.github.com>
2022-09-10 10:11:29 -0400

    Adds a basic Player Script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed = 10.0f;
+    float horizontal = 0.0f;
+    float vertical = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+    }
+}
