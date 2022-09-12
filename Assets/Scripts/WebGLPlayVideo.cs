cfa5c640a72cccf21779abefaf96405e8ef70dd3
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2022-09-12 18:39:04 -0400

    debug WebGL build by setting 1st scene to PlayStory scene

--- Assets/Scripts/WebGLPlayVideo.cs
+++ Assets/Scripts/WebGLPlayVideo.cs
@@ -4,15 +4,22 @@ using UnityEngine.Video;
 public class WebGLPlayVideo : MonoBehaviour
 {
     VideoPlayer videoPlayer;
-    [SerializeField] string url;
+    public string url;
+
+    public GameObject screen1;
+    public GameObject screen2;
+
     // Use this for initialization
     void Start()
     {
         if (Application.platform == RuntimePlatform.WebGLPlayer)
         {
-            videoPlayer = GetComponent<VideoPlayer>();
-            videoPlayer.url = url;
-            videoPlayer.Play();
+            screen1.SetActive(false);
+            screen2.SetActive(true);
+
+            //videoPlayer = GetComponent<VideoPlayer>();
+            //videoPlayer.url = url;
+            //videoPlayer.Play();
         }
     }
 }
