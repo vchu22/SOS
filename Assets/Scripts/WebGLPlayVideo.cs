6df7d2ea1fe55d243b362b7197c520e135827ead
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2022-09-12 18:25:07 -0400

    pushing WebGL fix

---
+++ Assets/Scripts/WebGLPlayVideo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class WebGLPlayVideo : MonoBehaviour
+{
+    VideoPlayer videoPlayer;
+    [SerializeField] string url;
+    // Use this for initialization
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+            videoPlayer.url = url;
+            videoPlayer.Play();
+        }
+    }
+}
