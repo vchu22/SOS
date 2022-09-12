01c2f1c125980f9cf9c2812abf6b1499bbb3f034
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2022-09-12 18:45:55 -0400

    debug WebGL build by setting 1st screen to PlayStory

--- Assets/Scripts/WebGLPlayVideo.cs
+++ Assets/Scripts/WebGLPlayVideo.cs
@@ -6,20 +6,14 @@ public class WebGLPlayVideo : MonoBehaviour
     VideoPlayer videoPlayer;
     public string url;
 
-    public GameObject screen1;
-    public GameObject screen2;
-
     // Use this for initialization
     void Start()
     {
         if (Application.platform == RuntimePlatform.WebGLPlayer)
         {
-            screen1.SetActive(false);
-            screen2.SetActive(true);
-
-            //videoPlayer = GetComponent<VideoPlayer>();
-            //videoPlayer.url = url;
-            //videoPlayer.Play();
+            videoPlayer = GetComponent<VideoPlayer>();
+            videoPlayer.url = url;
+            videoPlayer.Play();
         }
     }
 }
